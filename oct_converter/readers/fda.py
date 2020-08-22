495f91926574cc201fcedeef6c100b647cd74279
Mark <mark.graham@kcl.ac.uk>
2020-08-22 12:01:10 +0100

    Experimental support for extracting OCT  data from .fda files

--- oct_converter/readers/fda.py
+++ oct_converter/readers/fda.py
@@ -13,6 +13,7 @@ class FDA(object):
         Attributes:
             filepath (str): Path to .img file for reading.
             header (obj:Struct): Defines structure of volume's header.
+            oct_header (obj:Struct): Defines structure of OCT header.
             fundus_header (obj:Struct): Defines structure of fundus header.
             chunk_dict (dict): Name of data chunks present in the file, and their start locations.
     """
@@ -23,6 +24,16 @@ class FDA(object):
             'FDA' / PaddedString(3, 'ascii'),
             'version_info_1' / Int32un,
             'version_info_2' / Int32un
+        )
+
+        self.oct_header = Struct(
+            'unknown' / PaddedString(1, 'ascii'),
+            'width' / Int32un,
+            'height' / Int32un,
+            'bits_per_pixel' / Int32un,
+            'number_slices' / Int32un,
+            'unknown' / PaddedString(1, 'ascii'),
+            'size' / Int32un,
         )
 
         self.fundus_header = Struct(
@@ -65,6 +76,30 @@ class FDA(object):
         for key in chunk_dict.keys():
             print(key)
         return chunk_dict
+
+    def read_oct_volume(self):
+        """ Reads OCT data.
+
+            Returns:
+                obj:OCTVolumeWithMetaData
+        """
+
+        import matplotlib.pyplot as plt
+        if b'@IMG_MOT_COMP_03' not in self.chunk_dict:
+            raise ValueError('Could not find OCT header @IMG_MOT_COMP_03 in chunk list')
+        with open(self.filepath, 'rb') as f:
+            chunk_location, chunk_size = self.chunk_dict[b'@IMG_MOT_COMP_03']
+            f.seek(chunk_location) # Set the chunk’s current position.
+            f.seek(chunk_location)
+            raw = f.read(22)
+            oct_header = self.oct_header.parse(raw)
+            number_pixels = oct_header.width * oct_header.height * oct_header.number_slices
+            raw_volume = np.fromstring(f.read(number_pixels * 2), dtype=np.uint16)
+            volume = np.array(raw_volume)
+            volume = volume.reshape(oct_header.width, oct_header.height, oct_header.number_slices, order='F')
+            volume = np.transpose(volume, [1, 0, 2])
+        oct_volume = OCTVolumeWithMetaData([volume[:, :, i] for i in range(volume.shape[2])])
+        return oct_volume
 
     def read_fundus_image(self):
         """ Reads fundus image.
