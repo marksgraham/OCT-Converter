496b29ddca110d4af582c062d04e9963ad120bf1
Susannah Trevino <susannahtrevino@flywheel.io>
2023-10-25 14:34:46 -0500

    Update demos

--- examples/demo_optovue_extraction.py
+++ examples/demo_optovue_extraction.py
@@ -1,3 +1,4 @@
+from oct_converter.dicom import create_dicom_from_oct
 from oct_converter.readers import POCT
 
 filepath = "../sample_files/sample.OCT"
@@ -7,3 +8,10 @@ oct_volumes = poct.read_oct_volume()
 for volume in oct_volumes:
     volume.peek()  # plots a montage of the volume
 print("debug")
+
+# create DICOM from .OCT
+dcm = create_dicom_from_oct(filepath)
+# Output dir can be specified, otherwise will
+# default to current working directory.
+# If multiple volumes are identified within the file,
+# multiple DICOMs will be outputted.
